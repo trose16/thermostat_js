26817cd90e82cd5393acf8bd9d071caf87c7cbdc
Jack <jack_6894@hotmail.com>
2016-12-07 12:16:40 +0000

    added temperature increase of 5 & 1

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,10 +1,21 @@
 describe("Thermostat", function(){
     var thermostat;
+    beforeEach(function(){
+      thermostat = new Thermostat()
+    });
 
     describe("thermostat starts with:", function() {
       it("a default temperature of 20 degrees", function(){
-        thermostat = new Thermostat();
-        expect(thermostat.temperature()).toEqual(20)
+        expect(thermostat.currentTemperature).toEqual(20)
+      });
+    });
+
+    describe("thermostat can change temperature:", function(){
+      it("increases temperature by 1", function(){
+        expect(thermostat.increaseTemperature(1)).toEqual(21)
+      });
+      it("increases temperature by 5", function(){
+        expect(thermostat.increaseTemperature(5)).toEqual(25)
       });
     });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,6 +1,8 @@
 var Thermostat = function() {
-}
+  this.currentTemperature = 20
+};
 
-Thermostat.prototype.temperature = function(){
-  return 20
+Thermostat.prototype.increaseTemperature = function(number) {
+   this.currentTemperature += number
+   return this.currentTemperature
 };
